b153688137e3366c6d33933f10da02e313d8529e
jczetta <jczetta@gmail.com>
2015-06-13 15:44:39 -0400

    initial commit

---
+++ content/example.md
@@ -0,0 +1,9 @@
+Title: My super title
+Date: 2015-06-03 10:20
+Tags: thats, awesome
+Category: yeah
+Slug: my-super-post
+Author: jczetta
+Summary: Short version for index and feeds
+
+Test test test.
---
+++ pelicanconf.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'jczetta'
+SITENAME = u'let\'s live on a spaceship'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'USA/New York'
+
+DEFAULT_LANG = u'en'
+
+# Added to specify theme
+THEME = "pelican-themes/waterspill-en"
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('me!', 'http://jczetta.net/'),
+         ('coops!', 'icc.coop/'),
+         ('tags', 'therewillbealinklater.org'),)
+
+# Social widget
+SOCIAL = None#(('None', '#'),)
+
+DEFAULT_PAGINATION = 1
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
